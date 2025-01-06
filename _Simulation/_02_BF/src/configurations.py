d1011324a021e46a386119d0cbbc6d46dc2fb273
LuSchumacher <lu.schumacher@bluewin.ch>
2025-01-06 10:30:47 +0100

    update recovery study

--- _Simulation/_02_BF/src/configurations.py
+++ _Simulation/_02_BF/src/configurations.py
@@ -23,8 +23,8 @@ model_configs = {
         "type": "hyperbolic",
         "output_dims": 2,
         "param_names": [r"$v$", r"$a_0$", r"$\lambda$", r"$\tau$", r"$\sigma_z$"],
-        "prior_means": np.array([1.5 , 2.75, 1.05, 0.53]),
-        "prior_stds": np.array([0.86, 0.72, 0.55, 0.27])
+        "prior_means": np.array([1.5 , 2.75, 1.05, 0.53, 0.5]),
+        "prior_stds": np.array([0.86, 0.72, 0.55, 0.27, 0.29])
     },
     "exponential_ddm_no_contraint": {
         "name": "exponential_ddm_no_contraint",
@@ -34,7 +34,7 @@ model_configs = {
         "type": "exponential",
         "output_dims": 1,
         "param_names": [r"$v$", r"$a_0$", r"$\lambda$", r"$\tau$"],
-        "prior_means": np.array([1.5 , 2.75, 1.05, 0.53, 0.5]),
+        "prior_means": np.array([1.5 , 2.75, 1.05, 0.53]),
         "prior_stds": np.array([0.86, 0.72, 0.55, 0.27])
     },
     "exponential_ddm_ndt_contraint": {
